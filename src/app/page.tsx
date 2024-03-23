9c1acafe3aa91c1b92ed5e00b914edfffc79c362
TenpennyUMFPOS <med19aziz@gmail.com>
2024-03-23 11:38:07 +0000

    Regen Prisma

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default async function Home() {
   return (
     <main>
       <h1>SUPP</h1>
-      <Link href={"/pages/register"}> Register </Link>
+      <Link href={"/pages/register"}> REgister </Link>
 
     </main>
   );
