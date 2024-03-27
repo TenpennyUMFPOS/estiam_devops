233411e24abf683795be05db56009c08ee6c6a0b
TenpennyUMFPOS <med19aziz@gmail.com>
2024-03-27 09:22:28 +0000

    mrge dev

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -19,12 +19,7 @@ const Hero = () => {
                   wallet. The smartest way to buy, store, and swap crypto.²
                 </p>
                 <div className="flex flex-col items-left justify-left space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
-                  <Link
-                    href="register"
-                    className="rounded-sm bg-primary px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-primary/80"
-                  >
-                    Sign up
-                  </Link>
+
                 </div>
               </div>
             </div>
